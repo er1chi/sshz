aae8d38375bacb354a7ff5f5c13f0381ad2b9bdf
0xcire <ciredeveloper@gmail.com>
2026-04-24 16:04:35 -0400

    nitpick - me

--- v0/src/context/form-store.tsx
+++ v0/src/context/form-store.tsx
@@ -1,5 +1,5 @@
-import { createSimpleContext } from "@/utils/create-simple-context"
-import type { FocusField } from "@/types"
+import { createSimpleContext } from "@/utils/create-simple-context";
+import type { FocusField } from "@/types";
 
 interface SSH_Host {
   alias: string;
@@ -9,36 +9,33 @@ interface SSH_Host {
   identityFile: string;
 }
 interface FormState {
-  showModal: boolean
-  showOptions: boolean
-  focusedField: FocusField
-  isEditing: boolean
-  host: SSH_Host
-  original: SSH_Host
+  showModal: boolean;
+  showOptions: boolean;
+  focusedField: FocusField;
+  isEditing: boolean;
+  host: SSH_Host;
+  original: SSH_Host;
 }
+
+const blankHost = {
+  alias: "",
+  hostname: "",
+  user: "",
+  port: "",
+  identityFile: "",
+};
 
 const init: FormState = {
   showModal: false,
   showOptions: false,
   focusedField: "alias",
   isEditing: false,
-  host: {
-    alias: "",
-    hostname: "",
-    user: "",
-    port: "",
-    identityFile: "",
-  },
-  original: {
-    alias: "",
-    hostname: "",
-    user: "",
-    port: "",
-    identityFile: "",
-  },
-}
+  host: blankHost,
+  original: blankHost,
+};
 
 export const FormStore = createSimpleContext<FormState>({
   name: "Form",
   init,
-})
+});
+
